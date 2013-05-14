45dfa2423073b9cc09b2bd024361d05fdaf81cf2
Ayende Rahien <ayende@ayende.com>
2013-05-14 10:53:58 +0100

    Getting read for inheritence

---
+++ NHibernate.Course.London.May2013/Controllers/AnimalController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using NHibernate.Course.London.May2013.Models;
+
+namespace NHibernate.Course.London.May2013.Controllers
+{
+	public class AnimalController : NHibernateController
+	{
+		 public object Create()
+		 {
+			 Session.Save(new Animal
+				 {
+					 Name = "Generic Animal"
+				 });
+			 Session.Save(new Dog
+				 {
+					 Barks = true,
+					 Name = "Oscar"
+				 });
+			 Session.Save(new GermanShepherd
+				 {
+					 Name = "Arava",
+					 Barks = true,
+					 Purebread = true
+				 });
+
+			 Session.Save(new Cat
+				 {
+					 LastHairball = DateTime.Today,
+					 Name = "Go Away!"
+				 });
+
+			 return Json("Done");
+		 }
+
+		 public object QueryAnimals()
+		 {
+			 return Json(Session.QueryOver<Animal>().List());
+		 }
+
+		 public object QueryDogs()
+		 {
+			 return Json(Session.QueryOver<Dog>().List());
+		 }
+
+		 public object QueryCats()
+		 {
+			 return Json(Session.QueryOver<Cat>().List());
+		 }
+
+		 public object QueryGermanShepherd()
+		 {
+			 return Json(Session.QueryOver<GermanShepherd>().List());
+		 }
+	}
+}
---
+++ NHibernate.Course.London.May2013/Models/Animal.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace NHibernate.Course.London.May2013.Models
+{
+	public class Animal
+	{
+		public virtual int Id { get; set; }
+		public virtual string Name { get; set; }
+	}
+
+	public class Dog : Animal
+	{
+		public virtual bool Barks { get; set; }
+	}
+
+	public class GermanShepherd : Dog
+	{
+		public virtual bool Purebread { get; set; }
+	}
+
+	public class Cat : Animal
+	{
+		public virtual DateTime? LastHairball { get; set; }
+	}
+}
